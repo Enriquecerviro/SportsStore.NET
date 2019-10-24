a1ac33af31db0ed16a895839484d21eb244a0e10
enrique.cervino <enrique.cerviro@gmail.com>
2019-10-24 17:17:41 +0200

    empezamos administracion, quedan admin,seguridad y deployment

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -14,6 +14,41 @@ namespace SportsStore.Controllers
             repository = repoService;
             cart = cartService;
         }
+        /// <summary>
+        /// Lists this instance. Selecciona todos los objetos Order en el repo que tienen el
+        /// valor 'Shipped' en 'false' y los pasa a la vista por defecto. Este action method se
+        /// usará para pintar una lista de 'unShipped' pedidos para el administrador.
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+
+        /// <summary>
+        /// Marks the shipped. Recibe una peticion POST que especifica el ID de un pedido,
+        /// que es usado para localizar el objeto Order corrrespondiente del repositorio de
+        /// la forma que la propiedad 'Shipped' pueda ser seteada a true y guardada.
+        /// <para>
+        /// Para pintar la lista de unShipped orders, usare una vista razor con un elemento 
+        /// `table` para pintar algunos detalles, como qué productos se han comprado.
+        /// </para>
+        /// </summary>
+        /// <param name="orderID">The order identifier.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = repository.Orders
+                .FirstOrDefault(o => o.OrderID == orderID);
+            if( order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+
+
+
         public ViewResult Checkout() => View(new Order());
         [HttpPost]
         public IActionResult Checkout(Order order)
